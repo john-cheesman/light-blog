5634fc268e9573cb2ed716cb1cbc696e070a695b
John Cheesman <info@johncheesman.org.uk>
2014-03-24 13:40:19 +0000

    add spacing reset

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('styles', function() {
         .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(rename('style.css'))
-        .pipe(minifycss())
+        //.pipe(minifycss())
         .pipe(gulp.dest('css'))
         .pipe(notify({ message: 'Styles task complete' }));
 });
