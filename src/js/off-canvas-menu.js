65828f305e4558812d4fe6fb70b07e01b372258f
John Cheesman <info@johncheesman.org.uk>
2014-04-01 23:01:39 +0100

    working on base link styles

--- src/js/off-canvas-menu.js
+++ src/js/off-canvas-menu.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('click', function(event) {
         if ( matches.call( event.target, '.js-menu-link') ) {
             $('body').classList.toggle('menu-open');
-        }
 
-        event.preventDefault();
+            event.preventDefault();
+        }
     }, false);
 });
